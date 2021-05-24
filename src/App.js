a5eeb50311b82904fa5374320a212b8e573802dc
Khuldoonqataesheh <kqataesheh@gmail.com>
2021-05-24 16:57:15 +0300

    pulse check 8 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import List from './components/List';
+const todos = [{todo: "wake up", id: 1}, {todo: "breakfast", id: 2}, {todo: "study", id: 3}]
 
 
-// jsx
 const App = () => {
-  return <h1>MERAKI Academy</h1>;
+  const newTodos = todos.map((elem, i) => {
+    return <List key={elem.id} order={i} taskTitle={elem.todo} />;
+  });
+  return  <div>
+    <h1>hello world</h1>
+    {newTodos}
+    </div>;
 };
 
 export default App
---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from "react";
+const List = (props) => {
+  console.log("PROPS: ", props);
+  return (
+    <>
+      <ul className="Task"></ul>
+    </>
+  );
+};
+
+export default List;
